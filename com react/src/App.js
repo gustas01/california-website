1ca1de169629161cbc098bab108bcbe5f16afe96
gustas01 <gustasboyevolution@hotmail.com>
2022-03-28 18:06:32 -0300

    com react

--- com react/src/App.js
+++ com react/src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 import Navbar from'./Components/Navbar'
 import Content from './Components/Content'
+import Video from './video.mp4'
 
 
 function App() {
   return (
     
       <div className="hero">
-        <video loop muted playsInline className="back-video">
-          <source src="./video.mp4" type="video/mp4"/>
+        <video autoPlay loop muted plays-inline className="back-video">
+          <source src={Video} type="video/mp4"/>
         </video>        
         
         <Navbar />
